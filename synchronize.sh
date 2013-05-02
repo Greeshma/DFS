34573bdb4018dcf9861befd6b86dd0ca61a39a08
kln <nitheeshkl@gmail.com>
2013-05-03 03:04:29 +0530

    kln unison sync script

---
+++ synchronize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+basepath="/dfs/"
+baseserv=$1
+unison="/usr/bin/unison-2.27"
+
+cat mem_servers.txt | while read serv;do
+    src="ssh://$baseserv$basepath"
+    dest="ssh://$serv$basepath"
+    echo "echo synchronizing $src with $dest"
+    #$unison "$src" "$dest" -batch -auto -ui text
+done;
