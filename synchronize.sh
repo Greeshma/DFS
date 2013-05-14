13ade182ba4d4cdb32e420a806f01a45338f6265
greeshma.n <greeshma.n@flipkart.com>
2013-05-15 03:14:35 +0530

    Fixed some bugs in synchronise.sh

--- synchronize.sh
+++ synchronize.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-basepath="/dfs/"
+basepath="//dfs/"
 baseserv=$1
-unison="/usr/bin/unison-2.27"
+unison="/usr/bin/unison-2.40"
 
 cat mem_servers.txt | while read serv;do
     src="ssh://$baseserv$basepath"
     dest="ssh://$serv$basepath"
     echo "echo synchronizing $src with $dest"
-    #$unison "$src" "$dest" -batch -auto -ui text
+    unison "$src" "$dest" -batch -auto -ui text
 done;
-rm mem_servers.txt
